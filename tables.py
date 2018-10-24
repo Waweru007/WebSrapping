13f8b987961afb29466417ba153774ad794db575
mjwaweru007 <mjwaweru007@gmail.com>
2018-10-24 10:49:07 +0300

    Scrapig

--- tables.py
+++ tables.py
@@ -1,8 +1,13 @@
-import urllib3, urllib, nturl2path
-from urllib3 import poolmanager
-import bs4 as bs
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
 
-url="https://data.lacounty.gov/Parcel-/Assessor-Parcels-Data-2018/mk7y-hq5p"
+def make_soup(url):
+    thepage=urllib.request.urlopen(url)
+    soupdata=BeautifulSoup(thepage, "html.parser")
+    return soupdata
+soup=make_soup("https://data.lacounty.gov/Parcel-/Assessor-Parcels-Data-2018/mk7y-hq5p")
 
-
-
+for record in soup.findAll("outerContainer"):
+    for data in record.findAll("tr"):
+        print(data.text)
